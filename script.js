24431f9abead52d6490c7877c841f93bb220cde9
Marta Stepaniuk <martastepaniuk52@gmail.com>
2022-11-14 22:51:02 +0200

    changes

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let selectedNumber = document.getElementById('selectedNumber');
 let numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
     number.addEventListener('click', function () {
+        numbers.forEach(number => { number.classList.remove('active-state') });
         selectedNumber.innerHTML = number.innerHTML;
+        number.classList.add("active-state");
     });
 });
 submit.addEventListener('click', function () {
     design.style.display = 'none';
     thank_you.style.display = 'flex';
-
 });
